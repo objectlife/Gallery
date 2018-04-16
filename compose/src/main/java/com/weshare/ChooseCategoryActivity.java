911dbf866bdb84ea6c612b0af1bf3bfb69e4f44f
mr.simple <bboyfeiyu@gmail.com>
2018-04-16 16:09:37 +0800

    创建tag页面

--- compose/src/main/java/com/weshare/ChooseCategoryActivity.java
+++ compose/src/main/java/com/weshare/ChooseCategoryActivity.java
@@ -3,16 +3,23 @@ package com.weshare;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -33,15 +40,15 @@ import java.util.Random;
 
 public class ChooseCategoryActivity extends AppCompatActivity {
 
-    ImageView mPrevImageView ;
-    TextView mContentTextView ;
-    RecyclerView mCategoryRecyclerView ;
+    ImageView mPrevImageView;
+    TextView mContentTextView;
+    RecyclerView mCategoryRecyclerView;
     CategoryAdapter mAdapter = new CategoryAdapter();
 
     public static void start(Context context, Uri mediaUri, String text) {
         Intent starter = new Intent(context, ChooseCategoryActivity.class);
-        starter.putExtra("uri", mediaUri) ;
-        starter.putExtra("text", text) ;
+        starter.putExtra("uri", mediaUri);
+        starter.putExtra("text", text);
         context.startActivity(starter);
     }
 
@@ -51,23 +58,23 @@ public class ChooseCategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_category_layout);
 
-        mPrevImageView = findViewById(R.id.category_prev_imageview) ;
-        mContentTextView = findViewById(R.id.category_textview) ;
-        mCategoryRecyclerView = findViewById(R.id.category_recyclerview) ;
+        mPrevImageView = findViewById(R.id.category_prev_imageview);
+        mContentTextView = findViewById(R.id.category_textview);
+        mCategoryRecyclerView = findViewById(R.id.category_recyclerview);
 
-        Uri mediaUri = getIntent().getParcelableExtra("uri") ;
+        Uri mediaUri = getIntent().getParcelableExtra("uri");
         // todo : 要判断类别图片还是视频
-        if ( mediaUri != null ) {
+        if (mediaUri != null) {
             Picasso.with(this).load(mediaUri).into(mPrevImageView);
         }
 
         mContentTextView.setText(getIntent().getStringExtra("text"));
 
         for (int i = 0; i < 10; i++) {
-            Category category = new Category() ;
-            category.id = "id-" + i ;
-            category.name = "Word - " + i ;
-            mAdapter.mCategories.add(category) ;
+            Category category = new Category();
+            category.id = "id-" + i;
+            category.name = "Word - " + i;
+            mAdapter.mCategories.add(category);
         }
 
         mCategoryRecyclerView.setAdapter(mAdapter);
@@ -75,9 +82,78 @@ public class ChooseCategoryActivity extends AppCompatActivity {
         mAdapter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(view.getContext(), "click : " + mAdapter.mCategories.get(position), Toast.LENGTH_SHORT).show();
+                TagDialogFragment fragment = TagDialogFragment.newInstance() ;
+                fragment.show(getSupportFragmentManager(), TagDialogFragment.class.getSimpleName());
             }
         });
+    }
+
+    /**
+     *
+     */
+    public static class TagDialogFragment extends DialogFragment {
+
+        public static TagDialogFragment newInstance() {
+            Bundle args = new Bundle();
+            TagDialogFragment fragment = new TagDialogFragment();
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            Window window = getDialog().getWindow();
+            WindowManager.LayoutParams params = window.getAttributes();
+            params.gravity = Gravity.BOTTOM;
+            params.width = WindowManager.LayoutParams.MATCH_PARENT;
+            window.setAttributes(params);
+            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle
+                savedInstanceState) {
+            getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+            final View view = inflater.inflate(R.layout.tag_dialog_layout, container, false);
+            slideToUp(view, new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    initWidgets(view);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+            return view;
+        }
+
+        private void initWidgets(View itemView) {
+            itemView.findViewById(R.id.post_btn).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(v.getContext(), "click post button", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        private static void slideToUp(View view, Animation.AnimationListener listener) {
+            Animation slide = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+                    0.0f, Animation.RELATIVE_TO_SELF, 1.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+            slide.setDuration(300);
+            slide.setFillAfter(true);
+            slide.setFillEnabled(true);
+            slide.setAnimationListener(listener);
+            view.startAnimation(slide);
+        }
     }
 
 
@@ -86,15 +162,16 @@ public class ChooseCategoryActivity extends AppCompatActivity {
      */
     static class CategoryAdapter extends RecyclerView.Adapter<CategoryVH> {
 
-        static float[] sRadius = null ;
+        static float[] sRadius = null;
 
-        final List<Category> mCategories = new LinkedList<>() ;
-        AdapterView.OnItemClickListener mItemClickListener ;
+        final List<Category> mCategories = new LinkedList<>();
+        AdapterView.OnItemClickListener mItemClickListener;
         private Random random = new Random();
 
         @Override
         public CategoryVH onCreateViewHolder(ViewGroup parent, int viewType) {
-            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_item_layout, parent, false) ;
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_item_layout, parent,
+                    false);
             return new CategoryVH(itemView);
         }
 
@@ -104,9 +181,9 @@ public class ChooseCategoryActivity extends AppCompatActivity {
 
             GradientDrawable shape = new GradientDrawable();
             shape.setShape(GradientDrawable.RECTANGLE);
-            int radius = DisplayUtil.dip2px(holder.itemView.getContext(), 5) ;
-            if ( sRadius == null ) {
-                sRadius = new float[] { radius, radius, radius, radius, radius, radius, radius, radius } ;
+            int radius = DisplayUtil.dip2px(holder.itemView.getContext(), 5);
+            if (sRadius == null) {
+                sRadius = new float[]{radius, radius, radius, radius, radius, radius, radius, radius};
             }
             shape.setCornerRadii(sRadius);
             shape.setColor(Color.argb(255, random.nextInt(200), random.nextInt(200), random.nextInt(200)));
@@ -114,7 +191,7 @@ public class ChooseCategoryActivity extends AppCompatActivity {
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if ( mItemClickListener != null ) {
+                    if (mItemClickListener != null) {
                         mItemClickListener.onItemClick(null, v, position, position);
                     }
                 }
@@ -137,10 +214,11 @@ public class ChooseCategoryActivity extends AppCompatActivity {
      */
     static class CategoryVH extends RecyclerView.ViewHolder {
 
-        TextView nameTv ;
+        TextView nameTv;
+
         public CategoryVH(View itemView) {
             super(itemView);
-            nameTv = itemView.findViewById(R.id.category_name_tv) ;
+            nameTv = itemView.findViewById(R.id.category_name_tv);
         }
     }
 }
